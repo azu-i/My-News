c315e8348ad389e1790a6b7b1d76d2419aa6afd0
Azusa <ef.qkl.033v.0@gmail.com>
2020-10-09 12:51:17 +0000

    mynews

---
+++ resources/views/admin/news/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-wquiv="X-UA-Compatible" contact="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'addmin'], function(){
-      Route::get('news/create','Admin\NewsController@add');
-      Route::get('profile/create','Admin\ProfileController@add');
-      Route::get('profile/edit','Admin\ProfileController@edit');
+Route::group(['prefix'=>'admin'],function(){
+     Route::get('news/create','Admin\NewsController@add');
+     Route::get('profile/create','Admin\ProfileController@add');
+     Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
 

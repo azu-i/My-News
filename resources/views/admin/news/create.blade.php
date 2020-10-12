b830ac9195ce23dea4176cef5e38cbb174604c3a
Azusa <ef.qkl.033v.0@gmail.com>
2020-10-12 07:13:03 +0000

    mynews7

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-wquiv="X-UA-Compatible" contact="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    </head>
-    <body>
-        <!--<h1>Myニュース作成画面</h1>-->
+
         
         {{-- layouts/admin.blade.phpを読み込む --}}
         @extends('layouts.admin')
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-       <meta charset="utf-8">
-       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-       <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-    </head>
-    <body>
-         {{-- layouts/profile.blade.phpを読み込む --}}
-        @extends('layouts.profile')
+
+         {{-- layouts/admin.blade.phpを読み込む --}}
+        @extends('layouts.admin')
         
         {{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
         @section('title', 'プロフィール作成')
